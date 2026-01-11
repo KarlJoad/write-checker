68d0638dac0595cf66cc16f1ce6f252070370cd1
Karl Hallsby <karl@hallsby.com>
2026-01-11 14:59:39 -0600

    Add basic passive phrasing checking functionality
    If the user does not provide us a region to work with, then we check
    for passive phrases throughout the whole buffer.
    
    We use (interactive (if ...)) because we need to handle cases where
    the mark&point (region) have never been set in this session or when
    there is currently no active region. We also need to handle the case
    when a region is provided to us, but that is the easy path.

--- write-checker.el
+++ write-checker.el
@@ -91,5 +91,96 @@ The set of weasel words is defined by `write-checker-weasel-regexps'."
          list-matching-lines-default-context-lines
          `((,start . ,end))))
 
+
+;;;
+;;; Passive phrasing
+;;;
+
+(defcustom write-checker-passive-verb-regexps
+  '("am" "are" "were" "being" "is" "been" "was" "be")
+  "foo"
+  :group 'write-checker
+  :type '(repeat regexp))
+
+(defcustom write-checker-passive-adverb-regexps
+  '("awoken"
+    "been" "born" "beat"
+    "become" "begun" "bent"
+    "beset" "bet" "bid"
+    "bidden" "bound" "bitten"
+    "bled" "blown" "broken"
+    "bred" "brought" "broadcast"
+    "built" "burnt" "burst"
+    "bought" "cast" "caught"
+    "chosen" "clung" "come"
+    "cost" "crept" "cut"
+    "dealt" "dug" "dived"
+    "done" "drawn" "dreamt"
+    "driven" "drunk" "eaten" "fallen"
+    "fed" "felt" "fought" "found"
+    "fit" "fled" "flung" "flown"
+    "forbidden" "forgotten"
+    "foregone" "forgiven"
+    "forsaken" "frozen"
+    "gotten" "given" "gone"
+    "ground" "grown" "hung"
+    "heard" "hidden" "hit"
+    "held" "hurt" "kept" "knelt"
+    "knit" "known" "laid" "led"
+    "leapt" "learnt" "left"
+    "lent" "let" "lain" "lighted"
+    "lost" "made" "meant" "met"
+    "misspelt" "mistaken" "mown"
+    "overcome" "overdone" "overtaken"
+    "overthrown" "paid" "pled" "proven"
+    "put" "quit" "read" "rid" "ridden"
+    "rung" "risen" "run" "sawn" "said"
+    "seen" "sought" "sold" "sent"
+    "set" "sewn" "shaken" "shaven"
+    "shorn" "shed" "shone" "shod"
+    "shot" "shown" "shrunk" "shut"
+    "sung" "sunk" "sat" "slept"
+    "slain" "slid" "slung" "slit"
+    "smitten" "sown" "spoken" "sped"
+    "spent" "spilt" "spun" "spit"
+    "split" "spread" "sprung" "stood"
+    "stolen" "stuck" "stung" "stunk"
+    "stridden" "struck" "strung"
+    "striven" "sworn" "swept"
+    "swollen" "swum" "swung" "taken"
+    "taught" "torn" "told" "thought"
+    "thrived" "thrown" "thrust"
+    "trodden" "understood" "upheld"
+    "upset" "woken" "worn" "woven"
+    "wed" "wept" "wound" "won"
+    "withheld" "withstood" "wrung"
+    "written")
+  "blah"
+  :group 'write-checker
+  :type '(repeat regexp))
+
+;;;###autoload
+(defun write-checker-passive (start end)
+  "Check for passive phrasing in the region between START and END.
+
+If no region is active, the whole buffer is checked.
+
+A passive phrase id determined by the sequence of regexps from
+`write-checker-passive-verbs-regexps' and
+ `write-checker-passive-adverbs-regexps'."
+  (interactive (if (use-region-p)
+                   (list (region-beginning) (region-end))
+                 (list (point-min) (point-max))))
+  ;; FIXME: occur's regexp string matching only does character matching!
+  ;; We want to do whole-word matching, i.e. we want the regexp "is" to match
+  ;; the whole word "is" and NOT the 'i s' in "this".
+  (occur (concat "\\b\\(?:"
+                 (string-join write-checker-passive-verb-regexps "\\|")
+                 "\\)\\([[:space:]]\\|\\s<\\|\\s>\\)+\\(?:"
+                 (string-join write-checker-passive-adverb-regexps "\\|")
+                 "\\)\\b")
+         list-matching-lines-default-context-lines
+         `((,start . ,end))))
+
 (provide 'write-checker)
 ;;; write-checker.el ends here
